81532d54679d44e4d089251e70def9b771d88a39
Aleksandr Butukhanov <avbutukhanov@gmail.com>
2022-07-31 18:56:20 +0800

    initial commit

---
+++ src/test/java/ru/netology/test/CardOrderTest.java
@@ -0,0 +1,20 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTest {
+
+    @Test
+    void test() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79240000000");
+        $("[data-test-id=agreement]").click();
+        $("button[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
