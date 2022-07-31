87f0a914b5befbf686e60a170ccc49f3ce31e050
Aleksandr Butukhanov <avbutukhanov@gmail.com>
2022-07-31 19:45:32 +0800

    validation check

--- src/test/java/ru/netology/test/CardOrderTest.java
+++ src/test/java/ru/netology/test/CardOrderTest.java
@@ -17,4 +17,44 @@ public class CardOrderTest {
         $("button[type=button]").click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
+    @Test
+    void test1(){
+        open("http://localhost:9999/");
+//        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+79240000000");
+        $("[data-test-id=agreement]").click();
+        $("button[type=button]").click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void test2(){
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+//        $("[data-test-id=phone] input").setValue("+79240000000");
+        $("[data-test-id=agreement]").click();
+        $("button[type=button]").click();
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void test3(){
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79240000000");
+//        $("[data-test-id=agreement]").click();
+        $("button[type=button]").click();
+        $("[data-test-id=agreement].input_invalid").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
+
+    @Test
+    void test4(){
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Ivanov Ivan");
+        $("[data-test-id=phone] input").setValue("+79240000000");
+        $("[data-test-id=agreement]").click();
+        $("button[type=button]").click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
 }
